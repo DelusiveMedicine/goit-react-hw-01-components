4b97e9eeb1a7b0fac07dfa1bb5d9be63126205e9
Pagu-Chan <enjoyorhate@gmail.com>
2019-10-11 16:05:51 +0300

    opened build

---
+++ build/precache-manifest.9a9049daa0345eea036ca0fc9066d0d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee96b1a23f71d979b03daa159a5fb35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4986236a9dc9a3f19b2",
+    "url": "/static/css/main.7bbfebb4.chunk.css"
+  },
+  {
+    "revision": "1f530c7510bf4d4edb5d",
+    "url": "/static/js/2.2f91d972.chunk.js"
+  },
+  {
+    "revision": "f4986236a9dc9a3f19b2",
+    "url": "/static/js/main.4d7bc0d2.chunk.js"
+  },
+  {
+    "revision": "26a296d750338cbb486c",
+    "url": "/static/js/runtime-main.6695179a.js"
+  }
+]);
